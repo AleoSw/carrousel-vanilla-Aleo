c0e424c719a5d6bd02c6ac18062951826ef3ea65
AleoSw <98068231+AleoSw@users.noreply.github.com>
2022-04-17 20:04:44 -0500

    Update main.js

--- carousel/main.js
+++ carousel/main.js
@@ -1,39 +1,45 @@
-//scrollWidth: 2550
-//containerWidth: 1016
-//1534 = max ScrollLeft = scrollWidth - containerWidth
-
+//Capturamos los elementos necesarios del DOM
 const containerCarrousel = document.querySelector('.carrousel-container');
-const btnPrev = document.querySelector('.btn-prev');
-const btnNext = document.querySelector('.btn-next');
+const btnPrev = document.querySelector('.btn-prev'); // items = 'left'
+const btnNext = document.querySelector('.btn-next'); // items = 'right'
+//Agregamos el width de nuestras cards
 let cardWidth = 160;
+//Creamos una variable que nos permitira calcular el movimiento del scroll
 let scrollNum = 0;
 
-const carrousel = (direction) => {
+const carousel = (items) => {
+    //containerWidth es el ancho de nuestro contenedor que contiene nuestras Cards
     let containerWidth = containerCarrousel.clientWidth;
+    //ScrollWidth es el tamaño del Scroll de containerCarrousel
     let scrollWidth = containerCarrousel.scrollWidth;
+    //La resta de los anteriores nos dará un valor actualizable, que usaremos para dar un limite al carousel
     let maxScroll = scrollWidth - containerWidth;
 
-    if(direction == 'right') {
+    if(items == 'right') {
+        //Si scrollNum es menor a maxScroll la funcion se ejecutara sumando hacia la derecha el ancho de una Card
         if (maxScroll > scrollNum) {
             scrollNum += cardWidth;
             containerCarrousel.scroll(scrollNum, 0);
         }
     }
 
-    else if (direction == 'left') {
+    else if (items == 'left') {
+        //Si maxScroll es mayor a 0 la funcion se ejecutara moviendo restando hacia la izquierda el ancho de una Card
         if(maxScroll > 0) {
             scrollNum -= cardWidth;
             containerCarrousel.scroll(scrollNum, 0);
         }
     }
 
-    else if (direction == 'disableLeft') {
+    else if (items == 'disableLeft') {
+        //Si scrollNUm es menor que 160 el botonIzquierdo se desabilitará
         if(scrollNum < 160) {
             btnPrev.disabled = true;
         }
     }
 
-    else if(direction == 'disableRight') {
+    else if(items == 'disableRight') {
+        //Si scrollNum es igual a maxScroll el botonDerecho se desabilitará
         if(scrollNum > maxScroll) {
             btnNext.disabled = true;
         }
@@ -41,14 +47,15 @@ const carrousel = (direction) => {
 }
 
 btnNext.addEventListener ('click', () => {
-    carrousel('right');
-    carrousel('disableRight');
+    //Ejecuta la funcion y parametros
+    carousel('right');
+    carousel('disableRight');
     btnPrev.disabled = false;
 })
 
 btnPrev.addEventListener('click', () => {
-    carrousel('left');
-    carrousel('disableLeft');
+    //Ejecuta la funcion y parametros
+    carousel('left');
+    carousel('disableLeft');
     btnNext.disabled = false;
 })
-
